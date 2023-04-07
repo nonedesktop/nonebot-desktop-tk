62dcaafdd4b683f0977418464cb4e73200bdd5fa
worldmozara <worldmozara@163.com>
2023-04-08 00:04:46 +0800

    Add doc text

--- nonebot_desktop/gui.py
+++ nonebot_desktop/gui.py
@@ -822,7 +822,44 @@ def app_help():
     subw.title = "NoneBot Desktop - 使用手册"
 
     # W.I.P.
-    subw /= ()
+    nb = subw.add_widget(ttk.Notebook)
+    pg_home = nb.add_widget(tk.Label, justify="left", font=font10, wraplength=600)
+    pg_home.text = (
+        "欢迎使用 NoneBot Desktop 应用程序。\n\n"
+        "本程序旨在减少使用 NoneBot2 时命令行的使用。\n\n"
+        "这里包含了本程序的一些功能用法。\n"
+        "进入其它标签页查看更多。\n\n"
+        "提示：方括号 [] 包裹的内容与实际界面的控件文本相对应；\n"
+        "提示：尖括号 <> 包裹的内容表明其为外部应用程序；\n"
+        "提示：双左引号 `` 包裹的内容表示一个路径（统一使用 Unix 格式）。"
+    )
+    pg_create = nb.add_widget(tk.Label, justify="left", font=font10, wraplength=600)
+    pg_create.text = (
+        "本页介绍了如何使用本程序创建新项目。\n\n"
+        "在主界面点击 [项目]菜单 -> [新建项目] 进入创建项目页面。\n\n"
+        "在[项目目录]一栏 通过[浏览]选择一个目录 或 直接将路径粘贴至[输入框] 用于创建项目。\n"
+        "注意：项目目录*必须*是一个空目录（可以不存在），且避免使用保留名（如\u00a0nonebot\u00a0等）作为项目目录。\n\n"
+        "在[驱动器]一栏选择你需要的驱动器（通常是 [FastAPI]）。\n"
+        "注意：NoneBot2 项目需要*至少一个*驱动器才能正常工作！\n"
+        "提示：[None]\u00a0驱动器事实上相当于一个“空”驱动器，在不需要进行外部交互（如仅使用下面的\u00a0[Console]\u00a0适配器）"
+        "时可以提供占位。\n\n"
+        "适配器用于与外界进行特定协议的数据交换。\n"
+        "在[适配器]一栏选择你需要的适配器。\n"
+        "注意：NoneBot2 项目需要*至少一个*适配器才能正常工作！\n"
+        "提示：[OneBot V11] 与 [OneBot V12] 是两套不同的协议，它们互不兼容！\n"
+        "提示：如果要与 <go-cqhttp> 配合使用，应选择 [OneBot V11] 适配器。\n"
+        "提示：[Console] 适配器很适合做一些简单的测试。\n\n"
+        "如果需要使用无法从插件商店获取的插件（如自编插件、从源码下载的插件等），请勾选[预留配置用于开发插件]选项，"
+        "然后将这些插件正确放入 `src/plugins` 下。\n\n"
+        "[创建虚拟环境]可以有效避免因系统 Python 环境混乱造成的一系列问题，建议开启。\n\n"
+        "[自定义下载源]可以选择从不同的镜像站下载需要的程序包，一般可以加快下载速度。\n"
+        "注意：[https://pypi.org/simple] 是官方的下载源，更新及时但下载速度慢。\n"
+        "注意：无法保证使用时镜像源是否已同步最新的程序包，如果下载失败请更换不同的下载源。\n\n"
+        "创建完成后会自动进入新创建的项目目录。"
+    )
+    nb.base.add(pg_home.base, text="主页", padding=(2, 2))  # type: ignore
+    nb.base.add(pg_create.base, text="新建项目", padding=(2, 2))  # type: ignore
+    nb.pack(anchor="nw", expand=True, fill="both")
 
 
 def app_about():
